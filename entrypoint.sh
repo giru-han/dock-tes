b3799470f4c1f52cf7a9aaf940008232d8a0ca2c
Giru Haran <109772802+giru-han@users.noreply.github.com>
2024-05-17 16:32:09 +0800

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Set environment variable
+export SCRIPT_NAME="$SCRIPT_NAME"
+
+# Run the Python script
+python "$SCRIPT_NAME"
---
+++ mex.py
@@ -0,0 +1,5 @@
+import numpy as np
+import time
+k = np.random.rand(3,2)
+print(k[0,1])
+time.sleep(3)
