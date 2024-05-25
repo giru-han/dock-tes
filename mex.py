0fc7ee807eae7a07b9ce37a52d4c3da19c77dbee
Giru Haran <109772802+giru-han@users.noreply.github.com>
2024-05-25 11:49:00 +0800

    Update mex.py

--- mex.py
+++ mex.py
@@ -1,26 +1,26 @@
-import numpy as np
-import time
-import os
+#import numpy as np
+#import time
+#import os
 
-k = np.random.rand(3,2)
+#k = np.random.rand(3,2)
 #print(k[0,1])
 #print(os.getcwd())
 #time.sleep(3)
 
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 
 # Load environment variables from .env file
-load_dotenv()
+#load_dotenv()
 
 # Access environment variables
-secret_key = os.getenv("SECRET_KEY")
-database_url = os.getenv("DATABASE_URL")
+#secret_key = os.getenv("SECRET_KEY")
+#database_url = os.getenv("DATABASE_URL")
 
 # Use the environment variables in your script
 # For example:
-print(f"Secret key: {secret_key}")
-print(f"Database URL: {database_url}")
+#print(f"Secret key: {secret_key}")
+#print(f"Database URL: {database_url}")
 
 from flask import Flask, jsonify, request
 
